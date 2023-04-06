2b6122c39288010fa7af5535480203e936d8a99c
Nghia Tran <tvnghia174@gmail.com>
2023-04-06 14:29:35 +0700

    New project

---
+++ BabyFiew/BabyFiewApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyFiewApp.swift
+//  BabyFiew
+//
+//  Created by Nghia Tran on 06/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct BabyFiewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
