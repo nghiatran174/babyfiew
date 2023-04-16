20a5dc6d05fa45512ebf7839970d38de05720346
Nghia Tran <tvnghia174@gmail.com>
2023-04-16 21:22:50 +0700

    UI Feature Report

---
+++ BabyFiew/Report/viewModels/ReportViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ReportViewModel.swift
+//  BabyFiew
+//
+//  Created by Nghia Tran on 14/04/2023.
+//
+
+import Foundation
+
+class ReportViewModel : ObservableObject {
+    @Published var revenue = 0
+    
+    init(){
+        
+    }
+}
--- BabyFiew/Report/views/ReportView.swift
+++ BabyFiew/Report/views/ReportView.swift
@@ -8,13 +8,74 @@
 import SwiftUI
 
 struct ReportView: View {
+    @State var selectedTime: Time = .today
+    @ObservedObject var viewModel: ReportViewModel
+    
     var body: some View {
-        Text("Hello, report!")
+        GeometryReader { proxy in
+            VStack {
+                Text("Báo cáo doanh thu")
+                    .font(.title)
+                    .padding([.bottom], 20)
+                
+                Picker("Time", selection: $selectedTime){
+                    ForEach(Time.allCases, content: { time in
+                        Text(time.rawValue)
+                    })
+                }
+                .pickerStyle(.segmented)
+                .padding([.bottom], 10)
+                
+                let widthCard = proxy.size.width/2 - 50
+                HStack {
+                    ReportCardView(title: "Doanh thu", value: viewModel.revenue, backgroundColor: Color.green, width: widthCard)
+                    Spacer()
+                    ReportCardView(title: "Lượt bơi", value: viewModel.revenue, backgroundColor: Color.blue, width: widthCard)
+                }
+                
+                HStack {
+                    ReportCardView(title: "Số gói mới", value: viewModel.revenue, backgroundColor: Color.blue, width: widthCard)
+                    Spacer()
+                    ReportCardView(title: "Thông tin", value: viewModel.revenue, backgroundColor: Color.blue, width: widthCard)
+                }
+                
+//                Text("Doanh thu theo \(viewModel.titleRevenueType)")
+                
+                Spacer()
+            }
+            .padding()
+        }
     }
 }
 
 struct ReportView_Previews: PreviewProvider {
     static var previews: some View {
-        ReportView()
+        ReportView(viewModel: ReportViewModel())
+    }
+}
+
+enum Time: String, CaseIterable, Identifiable {
+    case today, week, month, all
+    var id: Self {self}
+}
+
+struct ReportCardView: View {
+    var title = "Title"
+    var value = 0
+    var backgroundColor = Color.white
+    var width: CGFloat = 50
+    
+    var body: some View {
+        VStack {
+            Label(title, systemImage: "dollarsign.circle")
+                .font(.title2)
+            Text(value.description)
+                .font(.title)
+        }
+        .frame(width: width, height: 40)
+        .padding()
+        .background(backgroundColor)
+        .cornerRadius(10)
+        .foregroundColor(Color.white)
     }
 }
